72cafb70114c52ee0a6e6db00bfe8913e822a21c
phdam100 <rightna@web.de>
2018-01-16 11:03:25 +0100

    add Lombok

--- src/main/java/entities/Profile.java
+++ src/main/java/entities/Profile.java
@@ -2,7 +2,6 @@ package entities;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
@@ -10,6 +9,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Profile {
 	
@@ -20,13 +22,13 @@ public class Profile {
 	private String gitname;
 	private String workSamples;
 	private String introText;
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "group_belonging", joinColumns = { @JoinColumn(name = "username") }, inverseJoinColumns = { @JoinColumn(name = "groupname") })
 	private List<Group> groupBelonging;
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "admin_of_groups", joinColumns = { @JoinColumn(name = "username") }, inverseJoinColumns = { @JoinColumn(name = "groupname") })
 	private List<Group> adminOfGroups;
-	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "group_application", joinColumns = { @JoinColumn(name = "username") }, inverseJoinColumns = { @JoinColumn(name = "groupname") })
 	private List<Group> groupApplication;
 	
